7ea356567f002db909b5ab6eccd78b1cd78d7def
Ryan Benson <ryan@obsidianforensics.com>
2014-06-23 09:57:35 -0700

    Rewrote extension from previous Perl version - instead of using a static list of the 1000 most popular extensions, this plugin reads the manifest / messages files to pull out the extension name and description.

---
+++ plugins/chrome_extensions.py
@@ -0,0 +1,32 @@
+###################################################################################################
+#
+# chrome_extensions.py
+#   Adds the name and description of each Chrome extension found in a URLItem to the Interpretation field
+#
+# Plugin Author: Ryan Benson (ryan@obsidianforensics.com)
+#
+###################################################################################################
+
+import re
+
+# Config
+friendlyName = "Chrome Extension Names"
+description = "Adds the name and description of each Chrome extension found in a URLItem to the Interpretation field"
+artifactTypes = ["url", "url (archived)"]
+remoteLookups = 0
+browser = "Chrome"
+browserVersion = 1
+version = "20140623"
+
+
+def plugin(target_browser):
+    extension_re = re.compile(r'^chrome-extension://([a-z]{32})')
+
+    for item in target_browser.parsed_artifacts:
+        if item.row_type in artifactTypes:
+            if item.interpretation is None:
+                m = re.search(extension_re, item.url)
+                if m:
+                    for ext in target_browser.installed_extensions:
+                        if ext.app_id == m.group(1):
+                            item.interpretation = "%s (%s) [Chrome Extension]" % (ext.name, ext.description)
