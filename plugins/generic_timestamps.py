bd03c0af4bf8f18973d41ca1ad9aa4f8a1f93335
Ryan Benson <ryan@obsidianforensics.com>
2014-06-30 13:44:32 -0700

    Reworked the generic epoch timestamp plugin to include Webkit timestamps as well, and I need to rename it.  Also expanded the plugin to process local storage (HTML5 cookies) as well.

---
+++ plugins/generic_timestamps.py
@@ -0,0 +1,34 @@
+###################################################################################################
+#
+# generic_timestamps.py
+#   If cookie data looks like it might be a timestamp, try to decode it
+#
+# Plugin Author: Ryan Benson (ryan@obsidianforensics.com)
+#
+###################################################################################################
+
+import re
+
+# Config
+friendlyName = "Generic Timestamp Decoder"
+description = "Attempts to detect and decode potential epoch second, epoch millisecond, and Webkit timestamps"
+artifactTypes = ["cookie (created)", "cookie (accessed)", "local storage"]
+remoteLookups = 0
+browser = "Chrome"
+browserVersion = 1
+version = "20140630"
+
+
+def plugin(target_browser):
+    timestamp_re = re.compile(r'^(1(\d{9}|\d{12}|\d{16}))$')
+    ls_timestamp_re = re.compile(r'timestamp.*?(\d{10,17})')
+
+    for item in target_browser.parsed_artifacts:
+        if item.row_type in artifactTypes:
+            if item.interpretation is None:
+                m = re.search(timestamp_re, item.value)
+                ls_m = re.search(ls_timestamp_re, item.value)
+                if m:
+                    item.interpretation = target_browser.friendly_date(int(m.group(0))) + " [potential timestamp]"
+                elif ls_m:
+                    item.interpretation = target_browser.friendly_date(int(ls_m.group(1))) + " [potential timestamp]"
