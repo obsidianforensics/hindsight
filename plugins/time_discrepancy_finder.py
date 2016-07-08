c5be0283f7fc5aa28eaf48d0548d5de31fb0b86b
Ryan <ryan@obsidianforensics.com>
2016-07-07 19:04:13 -0700

    Updated to add a few new cookies and modify the regex on some existing ones.

--- plugins/time_discrepancy_finder.py
+++ plugins/time_discrepancy_finder.py
@@ -23,7 +23,7 @@ artifactTypes = ("cookie (created)", "url")
 remoteLookups = 0
 browser = "any"
 browserVersion = 1
-version = "20141214"
+version = "20160707"
 parsedItems = 0
 
 
@@ -31,29 +31,32 @@ def plugin(target_browser):
 
     global parsedItems
     cookie_set = [
-        # website                   # cookie name           # regex for timestamp
-        {'.pubmatic.com/':          {'name': '_curtime',    'regex': r'(\d{10})'}},
-        {'.invitemedia.com/':       {'name': 'dp_rec',      'regex': r':(\d{10})}'}},
-        {'.atdmt.com/':             {'name': 'AA002',       'regex': r'^(\d{10})-'}},
-        {'.mathtag.com/':           {'name': 'mt_mop',      'regex': r'4:(\d{10})'}},
-        {'.33across.com/':          {'name': '33x_ps',      'regex': r'ts%3D(\d{13})'}},
-        {'.twitter.com/':           {'name': 'guest_id',    'regex': r'v1%3A(\d{13})'}},
-        {'.twitter.com/':           {'name': 'pid',         'regex': r'v3:(\d{13})'}},
-        {'www.yahoo.com/':          {'name': 'FBJSC',       'regex': r'(\d{10})'}},
-        {'.scorecardresearch.com/': {'name': 'UIDR',        'regex': r'(\d{10})'}},
-        {'.rubiconproject.com/':    {'name': 'put_3076',    'regex': r'(\d{10})'}},
-        {'.mookie1.com/':           {'name': 'mdata',       'regex': r'|(\d{10})$'}},
-        {'*':                       {'name': 'PREF',        'regex': r'LM=(\d{10})'}},
-        {'*':                       {'name': '__gads',      'regex': r'T=(1\d{9})'}},
-        {'.cbsnews.com/':           {'name': 'apexLat',     'regex': r'^(1\d{12})$'}},
-        {'.adblade.com/':           {'name': '__impt',      'regex': r'^(1\d{9})$'}},
-        {'.wtp101.com/':            {'name': 'cookie_born', 'regex': r'^(1\d{9})$'}},
-        {'.advertising.com/':       {'name': 'ACID',        'regex': r'^.{6}(1\d{9})'}},
-        {'.bidswitch.net/':         {'name': 'c',           'regex': r'^(1\d{9})$'}},
-        {'*':                       {'name': '__cfduid',    'regex': r'(1\d{9})$'}},
-        {'.insightexpressai.com/':  {'name': 'DW_Time',     'regex': r'^(1\d{9})$'}},
-        {'www.bose.com/':           {'name': 'bose_id',     'regex': r'^(1\d{12})'}},
-        {'.liveperson.net/':        {'name': 'LivePersonID','regex': r'd=(1\d{9})'}}
+        # website                   # cookie name            # regex for timestamp
+        {'.pubmatic.com/':          {'name': '_curtime',     'regex': r'(\d{10})'}},
+        {'.invitemedia.com/':       {'name': 'dp_rec',       'regex': r':(\d{10})}'}},
+        {'.atdmt.com/':             {'name': 'AA002',        'regex': r'^(\d{10})-'}},
+        {'.mathtag.com/':           {'name': 'mt_mop',       'regex': r'4:(\d{10})'}},
+        {'.33across.com/':          {'name': '33x_ps',       'regex': r'ts%3D(\d{13})'}},
+        {'.twitter.com/':           {'name': 'guest_id',     'regex': r'v1%3A(\d{13})'}},
+        {'.twitter.com/':           {'name': 'pid',          'regex': r'v3:(\d{13})'}},
+        {'www.yahoo.com/':          {'name': 'FBJSC',        'regex': r'(\d{10})'}},
+        {'.scorecardresearch.com/': {'name': 'UIDR',         'regex': r'(\d{10})'}},
+        {'.rubiconproject.com/':    {'name': 'put_3076',     'regex': r'(\d{10})'}},
+        {'.mookie1.com/':           {'name': 'mdata',        'regex': r'\|(\d{10})$'}},
+        {'*':                       {'name': 'PREF',         'regex': r'LM=(\d{10})'}},
+        {'*':                       {'name': '__gads',       'regex': r'T=(1\d{9})'}},
+        {'.cbsnews.com/':           {'name': 'apexLat',      'regex': r'^(1\d{12})$'}},
+        {'.adblade.com/':           {'name': '__impt',       'regex': r'^(1\d{9})$'}},
+        {'.wtp101.com/':            {'name': 'cookie_born',  'regex': r'^(1\d{9})$'}},
+        {'.advertising.com/':       {'name': 'ACID',         'regex': r'^.{6}(1\d{9})'}},
+        {'.bidswitch.net/':         {'name': 'c',            'regex': r'^(1\d{9})$'}},
+        {'*':                       {'name': '__cfduid',     'regex': r'(1\d{9})$'}},
+        {'.insightexpressai.com/':  {'name': 'DW_Time',      'regex': r'^(1\d{9})$'}},
+        {'www.bose.com/':           {'name': 'bose_id',      'regex': r'^(1\d{12})'}},
+        {'.liveperson.net/':        {'name': 'LivePersonID', 'regex': r'd=(1\d{9})'}},
+        {'.baidu.com/':             {'name': 'PSTM',         'regex': r'^(1\d{9})$'}},
+        {'.alexa.com/':             {'name': 'lv',           'regex': r'(1\d{9})$'}},
+        {'.doubleclick.net/':       {'name': 'id',           'regex': r'\|t=(1\d{9})\|'}}
     ]
 
     url_set = [
